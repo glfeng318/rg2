a04f3c579626dede36a534dc3fa601a766206fb8
glfeng <glfeng318@gmail.com>
2024-01-13 17:00:26 +0800

    fixed examples in g2_opt

--- R/opt.R
+++ R/opt.R
@@ -6,10 +6,11 @@
 #' @export
 #'
 #' @examples
-#' g2_opt(legend=FALSE)
-#' g2_opt(legend=FALSE, label=FALSE)
+#' g2_opt(list(), legend=FALSE)
+#' g2_opt(list(), legend=FALSE, label=FALSE)
 #'
 #' g2_opt(
+#'   list(),
 #'   color='rgb(252, 253, 191)-rgb(231, 82, 99)-rgb(183, 55, 121)',
 #'   pointStyle=list(
 #'     stroke='rgb(183, 55, 121)',
