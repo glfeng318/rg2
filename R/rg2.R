0e277fc61d1ab906b0b2ab5dd9e7f73163f45463
glfeng <glfeng318@gmail.com>
2024-01-17 23:52:03 +0800

    more examples

--- R/rg2.R
+++ R/rg2.R
@@ -847,6 +847,24 @@ g2_sankey <- function(data,sourceField,targetField,weightField,
 #' @inherit opt
 #' @family g2
 #' @export
+#' @examples
+#' data = jsonlite::fromJSON('[
+#' { "source": "北京", "target": "天津", "value": 30 },
+#' { "source": "北京", "target": "上海", "value": 80 },
+#' { "source": "北京", "target": "河北", "value": 46 },
+#' { "source": "北京", "target": "辽宁", "value": 49 },
+#' { "source": "北京", "target": "黑龙江", "value": 69 },
+#' { "source": "北京", "target": "吉林", "value": 19 },
+#' { "source": "天津", "target": "河北", "value": 62 },
+#' { "source": "天津", "target": "辽宁", "value": 82 },
+#' { "source": "天津", "target": "上海", "value": 16 },
+#' { "source": "上海", "target": "黑龙江", "value": 16 },
+#' { "source": "河北", "target": "黑龙江", "value": 76 },
+#' { "source": "河北", "target": "内蒙古", "value": 24 },
+#' { "source": "内蒙古", "target": "北京", "value": 32 }
+#' ]')
+#' g2_chord(data, 'source','target', 'value') |> g2()
+#'
 g2_chord <- function(data,sourceField,targetField,weightField){
   list(
     g2_chart='chord',
@@ -874,6 +892,9 @@ g2_treemap <- function(data,colorField,rawFields){
 #' @inherit opt
 #' @family g2
 #' @export
+#' @examples
+#' g2_violin(iris, 'Species','Sepal.Length') |> g2()
+#'
 g2_violin <- function(data,xField,yField,
                       seriesField=NULL){
   list(
